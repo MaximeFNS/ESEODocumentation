35e8c67e4a564b94c072fd91ccafa0e16cb235da
Maxime <maxime.afonso@reseau.eseo.fr>
2019-09-24 15:25:57 +0200

    Test commentaire

--- app/src/main/java/fr/eseo/dis/afonsodebarre/eseodocumentation/MainActivity.java
+++ app/src/main/java/fr/eseo/dis/afonsodebarre/eseodocumentation/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//Commentaire test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
