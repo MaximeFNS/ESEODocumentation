5290973f8661e4cd7b836df279c2e1b61619a428
Maxime <maxime.afonso@reseau.eseo.fr>
2019-10-01 16:04:44 +0200

    Formulaire de connexion

--- app/src/main/java/fr/eseo/dis/afonsodebarre/eseodocumentation/MainActivity.java
+++ app/src/main/java/fr/eseo/dis/afonsodebarre/eseodocumentation/MainActivity.java
@@ -3,6 +3,8 @@ package fr.eseo.dis.afonsodebarre.eseodocumentation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,8 +20,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button connect = (Button)findViewById(R.id.bn_connect);
+        connect.setEnabled(false);
         login = (EditText)findViewById(R.id.user_login);
         password = (EditText)findViewById(R.id.user_password);
+        login.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(login.getText()!=null && !login.getText().toString().equals("")){
+                    connect.setEnabled(true);
+                }
+                else{
+                    connect.setEnabled(false);
+                }
+            }
+        });
+
     }
 
     @Override
